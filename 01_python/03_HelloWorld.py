f133e38045ce850bebcad57ee10c25d6ff7c6321
yeoyun <ye0yunn@gmail.com>
2025-04-01 12:18:07 +0900

    .

---
+++ 01_python/03_HelloWorld.py
@@ -0,0 +1,19 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.16.7
+#   kernelspec:
+#     display_name: Python [conda env:base] *
+#     language: python
+#     name: conda-base-py
+# ---
+
+a1 = 10 + 20
+print(a1)
+print('Hello World!!!!')
+
+
---
+++ 01_python/04_jypyter_notebook_info.py
@@ -0,0 +1,50 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.16.7
+#   kernelspec:
+#     display_name: Python [conda env:base] *
+#     language: python
+#     name: conda-base-py
+# ---
+
+#
+#
+# ### 셀
+# - 설명이나 코드를 작성할 수 있는 칸 하나를 의미한다.
+
+# ### 셀 모드
+# - edit mode (enter) : 셀에서 엔터를 누르거나 셀을 클릭했을 경우, 현재 셀에 대한 편집 작업을 할 수 있다.
+# - command mode (esc) : jupyter에 있는 셀들을 관리할 수 있다.
+
+# ### 공통 단축키
+# - shift + enter : 현재 셀을 실행한다. 실행 후 다음 셀로 이동한다.
+# - ctrl + enter : 현재 셀을 실행한다. 실행 후 현재 셀에 머물러있다.
+
+# ### command mode 단축키
+# - 상하 방향키 : 편집하고자 하는 셀로 이동한다.
+# - a : 현재 셀 상단에 셀을 추가한다.
+# - b : 현재 셀의 하단에 셀을 추가한다.
+# - dd : 현재 셀을 삭제한다.
+# - m : 현재 셀을 markdown 모드로 변경한다.
+# - y : 현재 셀을 code 모드로 변경한다.
+# - z : 셀에 대한 작업에 대해 Undo
+# - shift + z : 셀에 대한 작업에 대해 Redo
+# - 셀들을 선택 한 후 shift + m : 선택한 셀들을 하나의 셀로 합쳐 준다.
+
+# ### edit mode 단축키
+# - 상하 방향키 : 현재 셀 제일 위에 있을 경우 이전 셀로 이동하고 현재 셀 제일 마지막에 있을 경우 다음 셀로 이동한다
+# - ctrl + shift + - : 현재 위치를 기준으로 셀을 나눈다.
+
+# # 제목1
+# ## 제목2
+# ### 제목3
+# #### 제목4
+# ##### 제목5
+# ###### 제목6
+
+
